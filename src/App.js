36c2a96201dbc5aefddba7d1db9d75cca2761d4c
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2020-09-21 01:38:11 +0200

    base react complete guide learning

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Components/Person';
+import AuthContext from './context/auth-context';
 
 function App() {
   const [showList, setShowList] = useState(false);
+  const [authe, setAuthe] = useState(false);
   const [data, setData] = useState([
-   {name: "ruben", age: 20},
-   {name: "rubenOld", age: 21},
-   {name: "rubenOlder", age: 22},
+    { name: 'ruben', age: 20 },
+    { name: 'rubenOld', age: 21 },
+    { name: 'rubenOlder', age: 22 },
   ]);
   let persons = null;
 
   const showPersonHandler = () => {
-    const res = showList===false ? true : false;
+    const res = showList === false;
     setShowList(res);
-  }
-  if(showList){
+  };
+
+  const loginHandler = () => {
+    setAuthe(!authe);
+  };
+
+  if (showList) {
     persons = (
       <div>
-        {data.map((person) => {
-        return (
+        {data.map((person) => (
           <Person name={person.name} age={person.age} />
-        )
-        })}  
+        ))}
       </div>
     );
-  
   }
 
   return (
     <div className="App">
       <h1>Learning React Complete Guide</h1>
-      <button onClick={showPersonHandler}>Show List/Hide List</button>
-      {persons}
+      <button type="button" onClick={loginHandler}>Login</button>
+      <button type="button" onClick={showPersonHandler}>
+        Show List/Hide List
+      </button>
+      <AuthContext.Provider
+        value={{
+          auth: authe,
+          login: loginHandler,
+        }}
+      >
+        {persons}
+      </AuthContext.Provider>
     </div>
   );
 }
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,16 +1,51 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../context/auth-context';
+import './styles.css';
 
 const Person = (props) => {
   const { name, age } = props;
+  // for functional component we call ->
+  const authContext = useContext(AuthContext);
+  // static contextType = AuthContext; better context & only class components type
+  // to call we just call context or this.context
+  /*
+  <AuthContext.Consumer>
+      {(context) => (
+        <div className="person">
+          {context.auth ? (
+            <p>User is Authenticated</p>
+          ) : (
+            <p>User is Not Authenticated</p>
+          )}
+          <h1>
+            Hello, my name is
+            {name}
+            , i am
+            {age}
+            Years Old!
+          </h1>
+        </div>
+      )}
+    </AuthContext.Consumer>
+    */
+  /* the following way is more simpler to use */
   return (
-    <h1>
-      Hello, my name is
-      {name}
-      , i am
-      {age}
-      Years Old!
-    </h1>
+    <div className="person">
+      {authContext.auth ? (
+        <p>User is Authenticated</p>
+      ) : (
+        <p>User is Not Authenticated</p>
+      )}
+      <h1>
+        Hello, my name is
+        {name}
+        , i am
+        {age}
+        Years Old!
+      </h1>
+    </div>
   );
 };
 
